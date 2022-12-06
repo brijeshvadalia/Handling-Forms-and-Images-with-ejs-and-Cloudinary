aeca8606ea959198501f13606c0dd4c2b064463a
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-06 12:03:52 +0530

    Added two new routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
@@ -6,10 +7,20 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : "/tmp/",
+}));
+
 
 app.get("/myget", (req, res) => {
     console.log(req.body);
+    res.send(req.body);
+  });
   
+app.post("/mypost", (req, res) => {
+    console.log(req.body);
+  console.log(req.files);
     res.send(req.body);
   });
   
